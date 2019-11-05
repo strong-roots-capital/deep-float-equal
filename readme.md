96c2750975d8ed41083b345d6f1c0cef800ec957
Eric Crosson <eric.s.crosson@utexas.edu>
2019-11-05 08:53:40 -0600

    BREAKING_CHANGE: compare arbitrary data structures
    instead of just arrays

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 > Check two objects for float-equality
 
+Note that `BigInt`s are currently not supported.
+
 ## Install
 
 ```shell
@@ -9,6 +11,24 @@ npm install @strong-roots-capital/deep-float-equal
 ```
 
 ## Use
+
+#### `deepFloatEqual<A, B>(a: A, b: B, absoluteTolerance = DBL_EPSILON, relativeTolerance = DBL_EPSILON`
+
+Checks if corresponding floats inside a pair of arbitrary
+data-structures are within the given tolerances of one another using
+the formula:
+
+|x - y| < max(absoluteTolerance, min(|x|, |y|) * relativeTolerance)
+
+- x and y are two numbers within the objects to comapre
+- absoluteTolerance is x fixed minimal tolerance (set to 0 to ignore)
+- relativeTolerance is x tolerance that scales with x/y (set to 0 to ignore)
+
+Returns true if x and y are approximately equal.
+
+If tolerance argument is omitted, almostEqual.DBL_EPSILON value is used by default.
+
+## Examples
 
 ```typescript
 import deepFloatEqual from '@strong-roots-capital/deep-float-equal'
@@ -19,7 +39,7 @@ console.log(deepFloatEqual(firstArray, secondArray))
 //=>true
 ```
 
-Optionally change the allowed epsilon tolerance
+Optionally change the allowed epsilon tolerance (default `DBL_EPSILON`)
 
 ```typescript
 import deepFloatEqual, { FLT_EPSILON } from '@strong-roots-capital/deep-float-equal'
@@ -32,9 +52,9 @@ console.log(deepFloatEqual(firstArray, secondArray, FLT_EPSILON / 1e10))
 
 `FLT_EPSILON` and `DBL_EPSILON` are re-exported from [almost-equal].
 
-## Related
+## Acknowledgments
 
 - [almost-equal]
-- [zip](https://github.com/strong-roots-capital/zip)
+- [fast-deep-equal](https://github.com/epoberezkin/fast-deep-equal)
 
   [almost-equal]: https://github.com/scijs/almost-equal
--- src/deep-float-equal.ts
+++ src/deep-float-equal.ts
@@ -2,37 +2,138 @@
  * deep-float-equal
  * Check two objects for float-equality
  */
+import Debug from 'debug'
+const debug = {
+    test: Debug('deep-float-equal')
+}
 
-import zip from '@strong-roots-capital/zip'
+import is from '@sindresorhus/is'
 import almostEqual from 'almost-equal'
 
 export { FLT_EPSILON, DBL_EPSILON } from 'almost-equal'
 
+
 /**
  * Check two objects for float-equality
- *
- * @remarks
- * Currently only supports deep-quality testing on arrays, not objects.
  *
  * @param a - First object to be compared `b`
  * @param b - Second object to be compared to `a`
  * @returns True if all floats in the objects are equivalent
  */
-export default function deepFloatEqual(
-    a: ReadonlyArray<number>,
-    b: ReadonlyArray<number>,
-    epsilon: number = almostEqual.FLT_EPSILON
+export default function deepFloatEqual<A, B>(
+    a: A,
+    b: B,
+    absoluteTolerance: number = almostEqual.DBL_EPSILON,
+    relativeTolerance: number = almostEqual.DBL_EPSILON
 ): boolean {
 
-    if (a.length !== b.length) {
-        return false
+    if ((a as any) === (b as any)) return true;
+
+    if (a && b && typeof a == 'object' && typeof b == 'object') {
+        if ((a as unknown as object).constructor !== (b as unknown as object).constructor) return false;
+
+        var length, i, keys;
+        if (Array.isArray(a)) {
+            if (!Array.isArray(b)) return false;
+            length = a.length;
+            if (length !== b.length) return false;
+            for (i = length; i-- !== 0;)
+                if (!deepFloatEqual(a[i], b[i], absoluteTolerance, relativeTolerance)) return false;
+            return true;
+        }
+
+        if (a instanceof Map) {
+            if (!(b instanceof Map)) return false;
+            if (a.size !== b.size) return false;
+            for(i of a.entries())
+                if (!b.has(i[0])) return false;
+
+            for(i of a.entries())
+                if (!deepFloatEqual(i[1], b.get(i[0]), absoluteTolerance, relativeTolerance)) return false;
+
+            return true;
+        }
+
+        if (a instanceof Set) {
+            if (!(b instanceof Set)) return false;
+            if (a.size !== b.size) return false;
+            for(i of a.entries())
+                if (!b.has(i[0])) return false;
+
+            return true;
+        }
+
+        if (is.int8Array(a) ||
+            is.uint8Array(a) ||
+            is.uint8ClampedArray(a) ||
+            is.int16Array(a) ||
+            is.uint16Array(a) ||
+            is.int32Array(a) ||
+            is.uint32Array(a) ||
+            is.float32Array(a) ||
+            is.float64Array(a) // ||
+            // is.bigInt64Array(a) ||
+            // is.bigUint64Array(a)
+           ) {
+
+            if (!is.int8Array(b) &&
+                !is.uint8Array(b) &&
+                !is.uint8ClampedArray(b) &&
+                !is.int16Array(b) &&
+                !is.uint16Array(b) &&
+                !is.int32Array(b) &&
+                !is.uint32Array(b) &&
+                !is.float32Array(b) &&
+                !is.float64Array(b) // &&
+                // !is.bigInt64Array(b) &&
+                // !is.bigUint64Array(b)
+               ) {
+                return false;
+            }
+
+            length = a.length;
+            if (length !== b.length) return false;
+            for (i = length; i-- !== 0;) {
+                const isEqual = almostEqual(a[i], b[i], absoluteTolerance, relativeTolerance)
+                debug.test(`${isEqual ? 'equal' : 'UNEQUAL'}: ${a} ~= ${b} with epsilon (${absoluteTolerance},${relativeTolerance})`)
+                if (!isEqual) return false;
+            }
+            return true;
+        }
+
+
+        if (is.regExp(a)) {
+            if (!is.regExp(b)) return false;
+            return a.source === b.source && a.flags === b.flags;
+        }
+        if ((a as unknown as object).valueOf !== Object.prototype.valueOf) return (a as unknown as object).valueOf() === (b as unknown as object).valueOf();
+        if ((a as unknown as object).toString !== Object.prototype.toString) return (a as unknown as object).toString() === (b as unknown as object).toString();
+
+        keys = Object.keys(a);
+        length = keys.length;
+        if (length !== Object.keys(b).length) return false;
+
+        for (i = length; i-- !== 0;)
+            if (!Object.prototype.hasOwnProperty.call(b, keys[i])) return false;
+
+        for (i = length; i-- !== 0;) {
+            var key = keys[i];
+            if (!deepFloatEqual((a as any)[key], (b as any)[key], absoluteTolerance, relativeTolerance)) return false;
+        }
+
+        return true;
     }
 
-    const zipped = zip(a, b)
-    for (const [x, y] of zipped) {
-        if (!almostEqual(x, y, epsilon, epsilon))
-            return false
+    if (Number.isNaN(a as unknown as number) && Number.isNaN(b as unknown as number)) return true;
+
+    if (is.number(a) && is.number(b)) {
+        const isEqual = almostEqual(a, b, absoluteTolerance, relativeTolerance)
+        debug.test(`${isEqual ? 'equal' : 'UNEQUAL'}: ${a} ~= ${b} with epsilon (${absoluteTolerance},${relativeTolerance})`)
+        return isEqual
     }
 
-    return true
+    // true if both NaN, false otherwise
+    return a!==a && b!==b;
 }
+
+//  LocalWords:  absoluteTolerance relativeTolerance isEqual
