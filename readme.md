b1d56401c3b8a81fe9d0ed835346127a15cda6e0
Eric Crosson <EricCrosson@users.noreply.github.com>
2019-11-05 09:04:16 -0600

    True deep equals (#4)
    * BREAKING_CHANGE: compare arbitrary data structures
    
    instead of just arrays

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 > Check two objects for float-equality
 
+Note that `BigInt`s are currently not supported.
+
 ## Install
 
 ```shell
@@ -9,6 +11,25 @@ npm install @strong-roots-capital/deep-float-equal
 ```
 
 ## Use
+
+#### `deepFloatEqual<A, B>(a: A, b: B, absoluteTolerance = DBL_EPSILON, relativeTolerance = DBL_EPSILON`)
+
+Checks for normal deep-equality between objects a and b with the
+exception that floats are allowed to vary within the given tolerances
+of one another using the formula:
+
+|x - y| < max(absoluteTolerance, min(|x|, |y|) * relativeTolerance)
+
+- x and y are two numbers within the objects to comapre
+- absoluteTolerance is x fixed minimal tolerance (set to 0 to ignore)
+- relativeTolerance is x tolerance that scales with x/y (set to 0 to ignore)
+
+Returns true if a, b are deep-equal, with all corresponding floats
+being approximately equal.
+
+If tolerance argument is omitted, almostEqual.DBL_EPSILON value is used by default.
+
+## Examples
 
 ```typescript
 import deepFloatEqual from '@strong-roots-capital/deep-float-equal'
@@ -19,22 +40,22 @@ console.log(deepFloatEqual(firstArray, secondArray))
 //=>true
 ```
 
-Optionally change the allowed epsilon tolerance
+Optionally change the allowed epsilon tolerance (default `DBL_EPSILON`)
 
 ```typescript
 import deepFloatEqual, { FLT_EPSILON } from '@strong-roots-capital/deep-float-equal'
 
 let firstArray = [0.1 + 0.2]
 let secondArray = [0.3]
-console.log(deepFloatEqual(firstArray, secondArray, FLT_EPSILON / 1e10))
+console.log(deepFloatEqual(firstArray, secondArray, FLT_EPSILON / 1e10, FLT_EPSILON / 1e10))
 //=>false
 ```
 
 `FLT_EPSILON` and `DBL_EPSILON` are re-exported from [almost-equal].
 
-## Related
+## Acknowledgments
 
 - [almost-equal]
-- [zip](https://github.com/strong-roots-capital/zip)
+- [fast-deep-equal](https://github.com/epoberezkin/fast-deep-equal)
 
   [almost-equal]: https://github.com/scijs/almost-equal
--- test/test-deep-float-equal.ts
+++ test/test-deep-float-equal.ts
@@ -1,4 +1,5 @@
-import test from 'ava'
+import test, { ExecutionContext } from 'ava'
+import { FLT_EPSILON, DBL_EPSILON } from 'almost-equal'
 
 /**
  * Library under test
@@ -6,38 +7,52 @@ import test from 'ava'
 
 import deepFloatEqual from '../src/deep-float-equal'
 
-test('should return true when array values are equal', t => {
-    let firstArray = [1, 2]
-    let secondArray = [1, 2]
-    t.true(deepFloatEqual(firstArray, secondArray))
-})
 
-test('should return true when array values have varying floats but still equal', t => {
-    let firstArray = [1.3456789, 100]
-    let secondArray = [1.345679, 100 + 1e-5]
-    t.true(deepFloatEqual(firstArray, secondArray))
-})
+function shouldBeEqual<A, B>(
+    t: ExecutionContext,
+    expected: boolean,
+    a: A,
+    b: B,
+    absoluteTolerance: number = DBL_EPSILON,
+    relativeTolerance: number = DBL_EPSILON
+) {
+    t.is(
+        expected,
+        deepFloatEqual(a, b, absoluteTolerance, relativeTolerance)
+    )
+}
+shouldBeEqual.title = <A, B>(
+    _ = '',
+    expected: boolean,
+    a: A,
+    b: B,
+    absoluteTolerance: number = DBL_EPSILON,
+    relativeTolerance: number = DBL_EPSILON
+) => `should${expected ? ' ' : ' not '}find that ${JSON.stringify(a)} deep-equals ${JSON.stringify(b)} with tolerance (${absoluteTolerance},${relativeTolerance})`
 
-test('should return false  when array values have varying floats', t => {
-    let firstArray = [1.3456789, 25.2334]
-    let secondArray = [1.345679, 25.2343]
-    t.false(deepFloatEqual(firstArray, secondArray))
-})
+test(shouldBeEqual, true, [1, 2], [1, 2])
+test(shouldBeEqual, true, [1.3456789, 100], [1.345679, 100 + 1e-5], FLT_EPSILON, FLT_EPSILON)
+test(shouldBeEqual, false, [1.3456789, 25.2334], [1.345679, 25.2343])
+test(shouldBeEqual, true, [0.1 + 0.2], [0.3])
+test(shouldBeEqual, false, [0.1 + 0.2], [0.3], FLT_EPSILON / 1e10, FLT_EPSILON / 1e10)
+test(shouldBeEqual, false, [0.1, 0.5], [0.1])
+test(shouldBeEqual, false, [0.1], [0.1, 0.6])
 
-test('should return true when comparing known float-inaccuracies ', t => {
-    let firstArray = [0.1 + 0.2]
-    let secondArray = [0.3]
-    t.true(deepFloatEqual(firstArray, secondArray))
-})
+test(shouldBeEqual, true, {a: 1.3456789, b: 100}, {a: 1.345679, b: 100 + 1e-5}, FLT_EPSILON, FLT_EPSILON)
+test(shouldBeEqual, false, {a: 1.3456789, b: 100}, {c: 1.345679, b: 100 + 1e-5})
+test(shouldBeEqual, false, {a: 1.3456789, b: 25.2334}, {a: 1.345679, b: 25.2343})
+test(shouldBeEqual, true, {horse: 0.1 + 0.2}, {horse: 0.3})
 
-test('should return false when b is shorter than a ', t => {
-    let firstArray = [0.1, 0.5]
-    let secondArray = [0.1]
-    t.false(deepFloatEqual(firstArray, secondArray))
-})
+test(shouldBeEqual, false, {a: 1.3456789, b: 100}, [1.345679, 100 + 1e-5])
+test(shouldBeEqual, true, {a: [1.3456789, 100], b: [0.1 + 0.2]}, {a: [1.345679, 100 + 1e-5], b: [0.3]}, FLT_EPSILON, FLT_EPSILON)
 
-test('should return false when a is shorter than b ', t => {
-    let firstArray = [0.1]
-    let secondArray = [0.1, 0.6]
-    t.false(deepFloatEqual(firstArray, secondArray))
-})
+test(shouldBeEqual, true, 100, 100 + 1e-5, FLT_EPSILON, FLT_EPSILON)
+test(shouldBeEqual, false, 1.3456789, 1.345679)
+
+test(shouldBeEqual, true, NaN, NaN)
+
+// TODO: test Map
+
+// TODO: test Set
+
+//  LocalWords:  absoluteTolerance relativeTolerance
